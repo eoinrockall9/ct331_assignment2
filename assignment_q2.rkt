6c53d51d90d04e73d283b145c67f622502fde907
Eoin Rockall <e.rockall9@gmail.com>
2018-11-04 11:48:51 +0000

    Q2 - First 3 Tests

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,17 +1,33 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
-
 (provide ins_beg)
+(provide ins_end)
+(provide cout_top_level)
+
 
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+  (cons el lst))
+
+(define (ins_end el lst)
+  (append lst (list el)))
+
+(define (cout_top_level lst)
+  (length (flatten lst)))
+
+
+(display "Tests for ins_beg:\n")
+
+(ins_beg 'a '(b c d))
+(ins_beg '(a b) '(b c d))
+
+(display "Test for ins_end:\n")
+(ins_end 'a '(b c d))
+(ins_end '(a b) '(b c d))
+
+(display "Test for cout_top_level:\n")
+(cout_top_level '(a b c (d e) (f g)))
+
+
+
+
+
