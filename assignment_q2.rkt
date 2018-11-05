e81a71bc885d50ce1920c14d9d761b51a7ead7ed
Eoin Rockall <e.rockall9@gmail.com>
2018-11-05 21:35:37 +0000

    Update to Q2 (nearly done)
    Rest of methods done in Q2

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -2,8 +2,10 @@
 
 (provide ins_beg)
 (provide ins_end)
-(provide cout_top_level)
-
+(provide count_top_level)
+(provide count_instances)
+(provide count_instances_tr)
+(provide count_instances_deep)
 
 (define (ins_beg el lst)
   (cons el lst))
@@ -11,9 +13,38 @@
 (define (ins_end el lst)
   (append lst (list el)))
 
-(define (cout_top_level lst)
+(define (count_top_level lst)
   (length (flatten lst)))
 
+(define (count_instances el lst)
+  (if (null? lst)
+      0
+      (if (equal? el (car lst))
+          (+ 1 (count_instances el (cdr lst)))
+          (count_instances el (cdr lst))
+      )
+  )
+)
+
+(define (tail el lst)
+  (count_instances_tr el lst 0))
+
+(define (count_instances_tr el lst)
+  (count_instances_tr el lst 0))
+(define (count_instances_tr_total el lst total) 
+  (cond [(empty? lst) total]
+    [(equal? (car lst) el) (count_instances_tr el (cdr lst) (+ 1 total))]
+    [else (count_instances_tr el (cdr lst) total)]))
+
+(define (count_instances_deep el lst)
+  (cond ((null? lst) 0)
+        ((equal? el (car lst))
+         (+ 1 (count_instances_deep el (cdr lst))))
+        ((list? (car lst))
+         (+ 0 (count_instances_deep el (cdr lst))
+             (count_instances_deep el (car lst))))
+        (else (count_instances_deep el (cdr lst)))
+))
 
 (display "Tests for ins_beg:\n")
 
@@ -24,8 +55,17 @@
 (ins_end 'a '(b c d))
 (ins_end '(a b) '(b c d))
 
-(display "Test for cout_top_level:\n")
-(cout_top_level '(a b c (d e) (f g)))
+(display "Test for count_top_level:\n")
+(count_top_level '(a b c (d e) (f g)))
+
+(display "Test for count_instances:\n")
+(count_instances 'a '(a b c d a f a))
+
+(display "Test for count_instances_tr:\n")
+(count_instances_tr 'a '(a b c d a f a))
+
+(display "Test for count_instances_deep:\n")
+(count_instances_deep 'a '( a b c '(a b a) c))
 
 
 
